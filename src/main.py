3eab11495d977440119a1d9a5d4aa3cf7d3debdc
Rohan Pandit <rohananandpandit@gmail.com>
2023-01-03 21:51:55 +0000

    Move sample code from function to main body

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from src.job import Job
 from src.scheduler import Scheduler, SchedulingOrder
 
 
-def SRPT():
+if __name__ == '__main__':
     jobs = []
     processing_times = [6, 4, 2, 1, 3, 2]
     release_times = [1, 2, 3, 4, 5, 6]
@@ -11,23 +11,6 @@ def SRPT():
         jobs.append(Job(i + 1, processing_time=p, release_time=r))
 
     for m in range(1, 4):
-        s = Scheduler(machines=m)
-        sch = s.shortest_remaining_processing_time(jobs.copy())
-        sch.plot(f'SRPT-{m}')
-        # print(f'tardiness: {sch.total_tardiness()}')
-
-
-if __name__ == '__main__':
-    SRPT()
-    # s = Scheduler(machines=5, order=SchedulingOrder.LIST)
-    # jobs = []
-    #
-    seed(11)
-    # for i in range(1, 10):
-    #     j = Job(i, processing_time=randint(1, 5), due_date=randint(1, 5))
-    #     # print(j.index, j.processing_time)
-    #     jobs.append(j)
-    #
-    # sch = s.wrap_around_rule(jobs)
-    # sch.plot('gantt')
-    # print(f'tardiness: {sch.total_tardiness()}')
+        s = Scheduler(machines=m, order=SchedulingOrder.LIST)
+        sch = s.longest_processing_time(jobs.copy())
+        sch.plot(f'gantt-chart-{m}')
